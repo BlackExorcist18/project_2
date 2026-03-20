b0390a34c2e5de2da9341a75995da51a7ff47978
Black Exorcist <bilbomistermaster@gmail.com>
2026-03-20 15:07:14 +0300

    Обновлена информация в файлах urls.py и views.py

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,97 @@
 from django.shortcuts import render
+from django.http import Http404
 
-# Create your views here.
+# Здесь будут ваши константы с данными
+from .data import COURSES, AUTHORS
+
+
+def index(request):
+    """Главная страница"""
+    return render(request, 'index.html')
+
+
+def courses(request):
+    """Страница со списком всех курсов"""
+    # Добавляем имя автора к каждому курсу для удобства отображения
+    courses_list = []
+    for course in COURSES:
+        # Находим автора курса
+        author = next((a for a in AUTHORS if a['id'] == course['author_id']), None)
+        course_copy = course.copy()
+        course_copy['author_name'] = author['name'] if author else 'Неизвестный автор'
+        courses_list.append(course_copy)
+    
+    context = {
+        'courses': courses_list,
+    }
+    return render(request, 'courses.html', context)
+
+
+def course_detail(request, course_id):
+    """
+    Страница конкретного курса
+    Используется view-параметр course_id
+    """
+    # Ищем курс по id
+    course = next((c for c in COURSES if c['id'] == course_id), None)
+    
+    if course is None:
+        raise Http404("Курс не найден")
+    
+    # Находим автора курса
+    author = next((a for a in AUTHORS if a['id'] == course['author_id']), None)
+    
+    # Добавляем дополнительные данные для шаблона
+    course_data = course.copy()
+    course_data['author_name'] = author['name'] if author else 'Неизвестный автор'
+    course_data['author_id'] = author['id'] if author else 0
+    
+    context = {
+        'course': course_data,
+    }
+    return render(request, 'course_detail.html', context)
+
+
+def authors(request):
+    """Страница со списком всех авторов"""
+    # Подсчитываем количество курсов для каждого автора
+    authors_list = []
+    for author in AUTHORS:
+        author_copy = author.copy()
+        # Считаем количество курсов автора
+        courses_count = sum(1 for c in COURSES if c['author_id'] == author['id'])
+        author_copy['courses_count'] = courses_count
+        authors_list.append(author_copy)
+    
+    context = {
+        'authors': authors_list,
+    }
+    return render(request, 'authors.html', context)
+
+
+def author_detail(request, author_id):
+    """
+    Страница конкретного автора
+    Используется view-параметр author_id
+    """
+    # Ищем автора по id
+    author = next((a for a in AUTHORS if a['id'] == author_id), None)
+    
+    if author is None:
+        raise Http404("Автор не найден")
+    
+    # Находим все курсы автора
+    author_courses = [c for c in COURSES if c['author_id'] == author_id]
+    
+    author_data = author.copy()
+    author_data['courses'] = author_courses
+    
+    context = {
+        'author': author_data,
+    }
+    return render(request, 'author_detail.html', context)
+
+
+def info(request):
+    """Информационная страница"""
+    return render(request, 'info.html')
--- my_project_2/urls.py
+++ my_project_2/urls.py
@@ -14,9 +14,26 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # Главная страница
+    path('index.html', views.index, name='index'),
+    path('', views.index),  # корневой URL тоже ведет на главную
+    
+    # Список курсов
+    path('courses.html', views.courses, name='courses'),
+    
+    # Страница конкретного курса (с параметром course_id)
+    path('course_<int:course_id>.html', views.course_detail, name='course_detail'),
+    
+    # Список авторов
+    path('authors.html', views.authors, name='authors'),
+    
+    # Страница конкретного автора (с параметром author_id)
+    path('author_<int:author_id>.html', views.author_detail, name='author_detail'),
+    
+    # Информационная страница
+    path('info.html', views.info, name='info'),
 ]
